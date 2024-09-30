ac928df1576bddcc1c1f2caf2f5884ea75f85694
TzuChaeDahy <viniciuslovestwice@gmail.com>
2024-09-30 20:09:22 -0300

    implement motion animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import Header from "./components/header/Header";
 import Intro from "./components/intro/Intro";
-import Photo from "./components/photo/Photo";
-import styles from "./style.module.css"
+import styles from "./style.module.css";
 
 export default function App() {
     return (
         <>
             <Header />
             <main className={styles.main}>
-                <section className={styles.first_section}>
-                    <Intro />
-                    <Photo />
-                </section>
+                <Intro />
             </main>
         </>
     );
--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -1,21 +1,46 @@
+import { motion, useAnimation, useInView } from "framer-motion";
 import { Github, Linkedin } from "lucide-react";
+import { useEffect, useRef } from "react";
 import { StatsProps } from ".";
 import Button from "../utils/button/Button";
 import styles from "./style.module.css";
 
-function Stats({ title, years, bordered = false }: StatsProps) {
-    const statsClass = bordered ? styles.stats + " " + styles.bordered : styles.stats;
-    return (
-        <div className={statsClass}>
-            <span className={styles.stats_years}>{years}+</span>
-            <span className={styles.stats_title}>{title}</span>
-        </div>
-    );
-}
+const presentationVariants = {
+    hidden: {
+        x: 75,
+        opacity: 0,
+    },
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            duration: 1.5,
+            type: "spring",
+            stiffness: 40,
+        },
+    },
+};
 
-export default function Intro() {
+function Presentation() {
+    const ref = useRef(null)
+    const isInView = useInView(ref, {once: true})
+    const motionControls = useAnimation();
+
+    useEffect(() => {
+        if (isInView) {
+            motionControls.start("visible")
+        }
+    }, [isInView]);
+
     return (
-        <article className={styles.wrapper}>
+        <motion.article
+            className={styles.intro_wrapper}
+            variants={presentationVariants}
+            initial="hidden"
+            animate={motionControls}
+            ref={ref}
+        >
             <div className={styles.presentation_wrapper}>
                 <span>Hi, i am</span>
                 <h2>Vinicius Alves</h2>
@@ -38,10 +63,67 @@ export default function Intro() {
                 />
             </div>
             <div className={styles.stats_wrapper}>
-                <Stats title="Years of xp" years={2}/>
-                <Stats title="Projects done" years={5} bordered/>
+                <Stats title="Years of xp" years={2} />
+                <Stats title="Projects done" years={5} bordered />
                 <Stats title="Happy clients" years={2} />
             </div>
-        </article>
+        </motion.article>
+    );
+}
+
+function Stats({ title, years, bordered = false }: StatsProps) {
+    const statsClass = bordered
+        ? styles.stats_container + " " + styles.bordered
+        : styles.stats_container;
+    return (
+        <div className={statsClass}>
+            <span className={styles.stats_years}>{years}+</span>
+            <span className={styles.stats_title}>{title}</span>
+        </div>
+    );
+}
+
+const photoVariants = {
+    hidden: {
+        x: -75,
+        opacity: 0,
+    },
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            duration: 1.5,
+            type: "spring",
+            stiffness: 40,
+        },
+    },
+};
+
+function Photo() {
+    return (
+        <motion.article
+            className={styles.photo_wrapper}
+            variants={photoVariants}
+            initial="hidden"
+            animate="visible"
+        >
+            <div className={styles.photo_background}>
+                <img
+                    src="/images/vinicius.png"
+                    alt="vinicius"
+                    className={styles.photo_image}
+                />
+            </div>
+        </motion.article>
+    );
+}
+
+export default function Intro() {
+    return (
+        <section className={styles.section}>
+            <Presentation />
+            <Photo />
+        </section>
     );
 }
